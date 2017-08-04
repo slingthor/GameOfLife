d44bebe8638d68535c311c32c6ecdf19c4e7ab01
Marianne Glad <marianneglad@mariannes-mbp>
2017-08-04 22:30:48 +0200

    Initial Commit

---
+++ GameOfLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameOfLife
+//
+//  Created by Marianne Glad on 8/4/17.
+//  Copyright © 2017 slingthor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GameOfLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameOfLife
+//
+//  Created by Marianne Glad on 8/4/17.
+//  Copyright © 2017 slingthor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
